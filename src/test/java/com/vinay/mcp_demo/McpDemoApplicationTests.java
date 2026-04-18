7b88dcc6970c6aa1f03676185f3d04cb678b0bc4
Vinay Yepuri <vinay.yepuri.12@gmail.com>
2026-04-18 16:24:18 +0530

    first commit

---
+++ src/test/java/com/vinay/mcp_demo/McpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinay.mcp_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
