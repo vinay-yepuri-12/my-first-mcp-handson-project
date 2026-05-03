7d3055708e3c5f44f71078c7c7562951d3ccb356
Vinay Yepuri <vinay.yepuri.12@gmail.com>
2026-05-03 12:05:08 +0530

    [vinay-branch-03052026 | Vinay] add github workflow file to run clean and build

--- src/main/java/com/vinay/mcp_demo/controller/HelloMcpController.java
+++ src/main/java/com/vinay/mcp_demo/controller/HelloMcpController.java
@@ -1,15 +1,34 @@
 package com.vinay.mcp_demo.controller;
 
+import com.vinay.mcp_demo.dto.MCPDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 @RestController
 @RequestMapping("/api")
 public class HelloMcpController {
 
+     private final List<MCPDto> namesList;
+
+    private final AtomicReference<BigInteger> counter =
+            new AtomicReference<>(BigInteger.ZERO);
+
+     public HelloMcpController() {
+         namesList = new ArrayList<>();
+     }
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello MCP";
+        BigInteger newValue = counter.updateAndGet(current -> current.add(BigInteger.ONE));
+        namesList.add(MCPDto.builder().name("ABC").age(newValue.toString()).build());
+        System.out.println(" INSIDE METHOD Hello " + newValue);
+        return "Hello MCP " + newValue;
     }
 }
---
+++ src/main/java/com/vinay/mcp_demo/dto/MCPDto.java
@@ -0,0 +1,13 @@
+package com.vinay.mcp_demo.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MCPDto {
+
+    private String name;
+    private String age;
+
+}
